084e86dff3178dd9d2bb7e099038e69e305d3b00
joe <snowjava13@gmail.com>
2018-07-17 21:13:15 -0500

    updated design, color, and function

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -6,11 +6,20 @@ public class Word {
 
     private String mMiwokTranslation;
 
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
 
     /**
      * Get the defualt translation of a word
@@ -25,4 +34,8 @@ public class Word {
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+
+    public int getImageResourceId() {return mImageResourceId;}
+
+    public boolean hasImage() {return mImageResourceId != NO_IMAGE_PROVIDED;}
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,18 +3,23 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -35,6 +40,21 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView defaultTextView = listItemView.findViewById(R.id.default_text_view);
 
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageResouceView = listItemView.findViewById(R.id.image_resource_view);
+
+        if (currentWord.hasImage()) {
+            imageResouceView.setImageResource(currentWord.getImageResourceId());
+            imageResouceView.setVisibility(View.VISIBLE);
+        } else {
+            imageResouceView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
